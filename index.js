6c7ee1ddcfd6b87d79d75ddfe442697a65229ec2
Purnama Hardi Saputra <82191219+purnamahardisaputra@users.noreply.github.com>
2023-02-09 22:31:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require("express");
+const welcome = require("./resources/views/welcome");
+
 const app = express();
-
-require("dotenv").config();
-
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const postsRouter = require("./routes/web");
-const authRouter = require("./routes/auth");
-
-app.use("/api/v1/web", postsRouter);
-app.use("/api/v1/auth", authRouter);
+app.use("/welcome", welcome);
 
 const PORT = process.env.PORT || 5000;
 
